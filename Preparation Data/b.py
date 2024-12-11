9696d7f4c011ef147218a3622a81c4d01c0912aa
Basant Magdy <91419497+76basant@users.noreply.github.com>
2024-12-11 10:58:13 +0200

    Update b.py

--- Preparation Data/b.py
+++ Preparation Data/b.py
@@ -1,1 +1,23 @@
-#preperation Data
+
+#How many Days
+from datetime import datetime
+
+# Define the start and end dates
+start_date = datetime(1976, 3, 1)
+end_date = datetime(2019, 12, 31)
+
+# Calculate the difference in days
+difference = (end_date - start_date).days
+print(difference)
+
+
+#How many months
+
+
+# Calculate the difference in months
+from dateutil.relativedelta import relativedelta
+
+# Compute the difference in months
+difference_months = relativedelta(end_date, start_date)
+total_months = difference_months.years * 12 + difference_months.months
+print(total_months)
